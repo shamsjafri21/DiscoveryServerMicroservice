94e58ac0b3f0d9ade78cf62a2ee3f6f8abc53ebf
shamsjafri21 <shams.jafri@teknikality.co.uk>
2021-09-06 05:13:02 -0700

    first commit

---
+++ src/test/java/com/teknikality/DiscoveryServerMicroservice/DiscoveryServerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teknikality.DiscoveryServerMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
